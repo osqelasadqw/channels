05bd0d4922cedf5ff2501b845e18f141eb0973b2
leqso7 <osqelan1@gmail.com>
2025-05-14 00:29:45 +0400

    sad

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,14 +7,14 @@ import { getDatabase } from 'firebase/database';
 
 // Firebase კონფიგურაცია
 const firebaseConfig = {
-  apiKey: "AIzaSyD92EjHS_Th7PfKiwZQyRbDDj6PmyLJTY0",
-  authDomain: "projec-cca43.firebaseapp.com",
-  databaseURL: "https://projec-cca43-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "projec-cca43",
-  storageBucket: "projec-cca43.appspot.com",
-  messagingSenderId: "198187490796",
-  appId: "1:198187490796:web:120d4fa367e26b21a274c9",
-  measurementId: "G-JQDC4K9T0K"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Firebase ინიციალიზაცია
