50631b50268bc1b96d5f98eca86943c31133a501
leqso7 <osqelan1@gmail.com>
2025-05-18 01:11:03 +0400

    sad

--- stripe/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-// სატესტო ფუნქცია — რომ დაინახო მუშაობს თუ არა ყველაფერი
-exports.helloWorld = functions.https.onRequest((req, res) => {
-  res.send("🔥 Firebase Function is working! Hello, Lexo!");
-});
